2c7d7f0710e0522fe0343a9ceb8cf70101fec71f
deep-quality-dev <hungrywarrior081@gmail.com>
2022-04-11 01:06:36 +1000

    feat: simple contract

---
+++ contracts/MyERC20.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "hardhat/console.sol";
+
+contract MyERC20 is ERC20, Ownable {
+    constructor() ERC20("MyERC20", "MYE") {
+        this;
+    }
+
+    function mintUpTo(address to, uint256 amount)
+        external
+        onlyOwner
+        returns (uint256)
+    {
+        uint256 currentBalance = balanceOf(to);
+        console.log("currentBalance", currentBalance);
+
+        if (currentBalance >= amount) return 0;
+
+        uint256 mintBalance = amount - currentBalance;
+        _mint(to, mintBalance);
+        console.log("_mint", mintBalance);
+
+        return mintBalance;
+    }
+
+    function addUp(uint256 a, uint256 b) external pure returns (uint256) {
+        return a + b;
+    }
+}
---
+++ contracts/MyOtherContract.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "./MyERC20.sol";
+
+contract MyOtherContract {
+    MyERC20 private myERC20;
+
+    uint256 public totalAmount;
+
+    constructor(MyERC20 myERC20_) {
+        myERC20 = myERC20_;
+    }
+
+    function myOtherFunction(address to, uint256 amount) external {
+        // do stuff
+
+        uint256 returnAmount = myERC20.mintUpTo(to, amount);
+        myERC20.mintUpTo(to, amount);
+        myERC20.mintUpTo(to, amount);
+
+        uint256 totalSupply = myERC20.totalSupply();
+        console.log("totalSupply", totalSupply);
+        console.log("The returned amount was", returnAmount);
+        console.log("The passed amount was", amount);
+
+        // do more stuff
+        totalAmount += amount;
+        console.log("totalAmount", totalAmount);
+    }
+}
